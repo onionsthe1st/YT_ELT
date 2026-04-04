bd5696e780a4fb19fc53ab2bceace47d2dc0cc69
Onions <kingsleyejiofor128@gmail.com>
2026-04-04 08:14:45 +0100

    extracted video ids

--- video_stats.py
+++ video_stats.py
@@ -4,8 +4,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="./.env")
+
 API_KEY = os.getenv("API_KEY")
 CHANNEL_NAME = "Judeinggg"
+max_results = 50
 
 def get_channel_id():
 
@@ -28,5 +30,39 @@ def get_channel_id():
     except requests.exceptions.RequestException as e:
         raise e
 
+
+
+
+def get_video_id(playlist_id):
+    video_ids=[]
+    pageToken = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={playlist_id}&key={API_KEY}" 
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+            data = response.json()    
+
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append(video_id)   
+
+            pageToken = data.get("nextPageToken")
+            if not pageToken:
+                break
+        return video_ids
+    except requests.exceptions.RequestException as e:
+        raise e
+
+len_video_ids = len(get_video_id(get_channel_id()))
+print(f"Total number of videos: {len_video_ids}")
+
 if __name__ == "__main__":
-    get_channel_id()    
+    playlist_id = get_channel_id()  
+    print(get_video_id(playlist_id))
